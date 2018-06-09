5345e646384872ee8c4689d2c424c3bad97e9e9b
Nguyen Thi Hai Anh <haianhnt.st@gmail.com>
2018-06-09 14:53:25 +0700

    Exercise #1

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const http = require("http");
 const morgan = require("morgan");
 const bodyParser = require('body-parser');
-const dishRounter = require("./routes/dishRouter");
+const dishRouter = require("./routes/dishRouter");
+const promoRouter = require("./routes/promoRouter");
+const leaderRouter = require("./routes/leaderRouter");
 
 const hostname = "localhost";
 const port = 3000;
@@ -11,7 +13,9 @@ const app = express();
 app.use(morgan("dev"));
 
 app.use(bodyParser.json());
-app.use("/dishes", dishRounter);
+app.use("/dishes", dishRouter);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
 
 app.use(express.static(__dirname + "/public")); 
 
